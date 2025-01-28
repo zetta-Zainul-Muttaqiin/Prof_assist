30a8b2f27fb79623ddd00015219728222f6d4791
fawazatha <132468764+fawazatha@users.noreply.github.com>
2025-01-28 14:34:51 +0700

    up

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 from dotenv import load_dotenv
 import logging
 import os
+import streamlit as st
 
 logging.basicConfig(
     filename='error.log', # Set a file for save logger output 
@@ -13,14 +14,19 @@ logging.basicConfig(
 LOGGER = logging.getLogger(__name__)
 LOGGER.info("Init Global Variable")
 
-# ********* load .env content
-load_dotenv(override=True)
 # ********* set token for openai and datastax
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+api_key = st.secrets["api"]
+OPENAI_API_KEY = api_key["OPENAI_API_KEY"]
 
-ASTRADB_TOKEN_KEY = os.getenv("ASTRADB_TOKEN_KEY")
-ASTRADB_API_ENDPOINT = os.getenv("ASTRADB_API_ENDPOINT")
-ASTRADB_COLLECTION_NAME = os.getenv("ASTRADB_COLLECTION_NAME")
+ASTRADB_TOKEN_KEY = api_key["ASTRADB_TOKEN_KEY"]
+ASTRADB_API_ENDPOINT = api_key["ASTRADB_API_ENDPOINT"]
+ASTRADB_COLLECTION_NAME = api_key["ASTRADB_COLLECTION_NAME"]
+ASTRADB_COLLECTION_NAME_UPLOAD_DOC = api_key["ASTRADB_COLLECTION_NAME_UPLOAD_DOC"]
+
+DB_FILE = api_key['DATABASE_CHAT_TOPIC']
+LIST_DOC_FILE = api_key["DATA_LIST_DOC"]
+
+URL_WEBHOOK = api_key['URL_WEBHOOK']
 
 GREETINGS_EN = ['hello', 'hi', 'hey', 'good morning', 'good afternoon', 'good evening', 'hi, how is it going?', 'greetings!', 'how are you doing?', 'how do you do?', 'what`s up?']
 GREETINGS_FR = ['bonjour', 'salut', 'coucou', 'bonsoir', 'bonjour à tous', 'comment allez-vous ce matin ?', 'bonne journée', 'bonne soirée', 'bonne nuit', 'À bientôt', 'À plus tard', 'À tout à lheure', 'À demain', 'Ça va?', 'enchanté']
